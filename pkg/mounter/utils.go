c6c70485013513cd006d09d4eb2b2c4e87258b09
lsundararajan <lsundararajan@purestorage.com>
2022-11-24 06:37:45 +0000

    win util
    Signed-off-by: lsundararajan <lsundararajan@purestorage.com>

---
+++ pkg/mounter/utils.go
@@ -0,0 +1,20 @@
+//go:build windows
+// +build windows
+package mounter
+
+import (
+	"os"
+	"os/exec"
+
+	"k8s.io/klog/v2"
+)
+
+const MaxPathLengthWindows = 260
+
+func RunPowershellCmd(command string, envs ...string) ([]byte, error) {
+	cmd := exec.Command("powershell", "-Mta", "-NoProfile", "-Command", command)
+	cmd.Env = append(os.Environ(), envs...)
+	klog.V(8).Infof("Executing command: %q", cmd.String())
+	out, err := cmd.CombinedOutput()
+	return out, err
+}
